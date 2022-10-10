c094f4e95b0f289b6ce6af43a733b6cd2aff16d7
Yousuf A. Khan <YousufAKhan1996@gmail.com>
2022-10-10 15:34:20 -0700

    Add files via upload

---
+++ add_noise.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import os
+import sys
+from Bio.PDB import *
+import numpy as np
+parser = PDBParser()
+
+
+# In[5]:
+
+
+def generate_random_splits(noise_size):
+    """
+    Description:
+        Generate a list of noise [x,y,z] to add to an atom coordinate
+    Input:
+        -noise_size: The amount of total noise desired in angstroms
+    Returns:
+        -noise_add: A list [x,y,z] of the noise to add to a coordinate
+    """
+    x_rand = np.random.uniform()
+    y_rand = np.random.uniform()
+    z_rand = np.random.uniform()
+    sum_rand = x_rand + y_rand + z_rand
+    factor = noise_size / sum_rand
+    x_scale = x_rand * factor
+    y_scale = y_rand * factor
+    z_scale = z_rand * factor
+    noise_add = [x_scale, y_scale, z_scale]
+    return noise_add
+
+
+# In[ ]:
+
+
+def generate_noised_pdb(structure, noise, path, n):
+    """
+    Description:
+        Write noised PDB file to the output path
+    Input:
+        -structure: The parsed PDB structure with biopython
+        -noise: The amount of noise (angstroms) that is desired
+        -path: The output directory
+        -n: The amount of noised PDB files to generate
+    Returns:
+    """
+    for i in range(n):
+        print('Processing '+str(i)+' out of '+str(n))
+        for model in structure:
+            for chain in model:
+                for res in chain:
+                    for atom in res:
+                        atom.set_coord(atom.coord + generate_random_splits(noise))
+        io = PDBIO()
+        io.set_structure(structure)
+        filename = str(structure.id)+'_noised_'+str(i)+'.pdb'
+        fp = os.path.join(path,filename)
+        io.save(fp)
+
+
+# In[ ]:
+
+
+def main():
+    # Take the inputs
+    input_file = sys.argv[1]
+    output_dir = sys.argv[2]
+    noise = float(sys.argv[3])
+    n = int(sys.argv[4])
+    structure = parser.get_structure("structure", input_file)
+    generate_noised_pdb(structure, noise, output_dir, n)
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    main()
+
